e095fbddddf410ace2a2444b472bcd2ec7e2efea
Alexander Werner <awerner@uwaterloo.ca>
2021-07-12 19:49:31 -0400

    more rename

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "R2D2 Competition Docker Image"
+echo "Roboethics Competition Docker Image"
 echo "If you close this window, all applications in the docker will be terminated"
 bash
 
